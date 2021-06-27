cf46b9ed72d344554d9646394e0adfc1924c94b7
Myfithub12 <85585871+Myfithub12@users.noreply.github.com>
2021-06-27 09:07:25 -0400

    Add files via upload

---
+++ Project/java/site.js
@@ -0,0 +1,13 @@
+// This is my first java
+
+function setupCounter( val ) {
+    return functioncounter()  {
+        return val++;
+    }
+}
+let counter1 = setupCounter(0);
+console.log(counter1());     //0
+console.log(counter1());     //1
+let counter2 = setupCounter(0);
+console.log(counter2());     //10
+console.log(counter2());     
