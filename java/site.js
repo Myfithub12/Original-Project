74d59259c62d77fa8c426e7aec1a09041dab2c0d
Mark <mhollrah01@yahoo.com>
2021-07-30 07:28:32 -0400

    0

--- java/site.js
+++ java/site.js
@@ -22,3 +22,12 @@ function Click(data, i) {
         }
     }, 100);
 }
+
+const consoleLinks = document.querySelectorAll("#tv #hamitems a");
+const consoleImage = document.querySelector("#console-image");
+
+for (link of consoleLinks) {
+    link.addEventListener("mouseover", (e) => {
+        consoleImage.src = e.target.href;
+    })
+}
